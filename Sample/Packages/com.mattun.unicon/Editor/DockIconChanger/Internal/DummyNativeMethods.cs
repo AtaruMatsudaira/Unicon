caa699d0d90b5b724f09402068ced25be9cfe184
Yuya Furuno <yakisabananoka@users.noreply.github.com>
2025-11-29 04:08:11 +0900

    add: WindowsSupport

---
+++ Sample/Packages/com.mattun.unicon/Editor/DockIconChanger/Internal/DummyNativeMethods.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+namespace DockIconChanger
+{
+    internal sealed class DummyNativeMethods : INativeMethods
+    {
+        public bool SetIconFromPath(string imagePath)
+        {
+            Debug.LogWarning("DockIconChanger: This feature is only available on macOS Editor and Windows Editor");
+            return false;
+        }
+
+        public bool SetIconWithColorOverlay(Color color)
+        {
+            Debug.LogWarning("DockIconChanger: This feature is only available on macOS Editor and Windows Editor");
+            return false;
+        }
+
+        public bool ResetIcon()
+        {
+            Debug.LogWarning("DockIconChanger: This feature is only available on macOS Editor and Windows Editor");
+            return false;
+        }
+    }
+}
---
+++ Sample/Packages/com.mattun.unicon/Editor/DockIconChanger/Internal/INativeMethods.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+namespace DockIconChanger
+{
+    internal interface INativeMethods
+    {
+        bool SetIconFromPath(string imagePath);
+        bool SetIconWithColorOverlay(Color color);
+        bool ResetIcon();
+    }
+}
